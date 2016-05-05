0008ce8f7667e1a713e6609c2ac79659e1a370d9
Alex <petrewoo@hotmail.com>
2016-05-06 00:11:28 +0800

    realize basic command of zookeeper client(python)

--- Hydra.py
+++ Hydra.py
@@ -2,70 +2,100 @@
 # encoding: utf-8
 
 from kazoo.client import KazooClient
+import kazoo.security as ks
 import logging
 import readline
 import json
+import pdb, traceback, sys
 
-# TODO resolve the double logging problem
-# TODO dig logging module
-FORMAT='%(asctime)s %(name)s %(levelname)s %(message)s'
-FORMAT1='%(levelname)s %(message)s'
-#  logging.basicConfig(format=FORMAT1)
-loggerM = logging.getLogger('Hydra')
+#  logging.basicConfig(level=logging.DEBUG)
+FORMAT = '%(asctime)s %(name)s %(levelname)s %(message)s'
+loggerM = logging.getLogger('Hydra.')
 #  loggerM.setLevel(logging.DEBUG)
-loggerM.setLevel(logging.WARN)
+loggerM.setLevel(logging.INFO)
 handler = logging.StreamHandler()
+fmt = logging.Formatter(FORMAT)
+handler.setFormatter(fmt)
 loggerM.addHandler(handler)
-COMMANDSET = ('ls', 'create', 'delete', 'set', 'init', 'up')
-# TODO add a configure file for host
+COMMAND_SET = ('ls', 'add', 'create', 'delete', 'rmr', 'set', 'setAcl', 'get', 'getAcl', 'init', 'quit')
 CONFIG_FILE = 'zkSerconfig.json'
 
 
 class Mykazoo(KazooClient):
-    def ls(self, path):
-        loggerM.debug('ls path is {}'.format(path))
+    # TODO full command set just like ZooKeeper client
+    def ls(self, *args):
+        loggerM.debug('ls path is {}'.format(args[0]))
         try:
-            attrList = self.get_children(path)
-            for attr in attrList:
-                value = self.get(path + '/' + attr)
-                print('{}: {}'.format(attr, value[0]))
+            attr_list = self.get_children(args[0])
+            attr_list.sort()
+            for attr in attr_list:
+                print(attr)
         except:
+            loggerM.warn('{} try get_children wrong'.format(args[0]))
             pass
 
-    def gls(self, path):
-        attrList = self.get_children(path)
-        loggerM.debug('gls got attrList {}'.format(attrList))
-        return attrList
+    def _gls(self, *args):
+        attr_list = self.get_children(args[0]) or []
+        return attr_list
 
-    def create(self, path):
-        if not self.exists(path):
-            self.ensure_path(path)
-        else:
-            print("Already exists...")
+    def set(self, *args):
+        super(Mykazoo, self).set(args[0], args[1])
 
-    def delete(self, path):
-        if not self.exists(path):
-            print "node is empty"
+    def setAcl(self, *args):
+        super(Mykazoo, self).set_acls(args[0], self.default_acl)
+
+    def get(self, *args):
+        print('{}'.format(super(Mykazoo, self).get(args[0])[0]))
+
+    def getAcl(self, *args):
+        print('{}'.format(super(Mykazoo, self).get_acls(args[0])[0]))
+
+    def delete(self, *args):
+        loggerM.info('delete path is {}'.format(args[0]))
+        if not self.exists(args[0]):
+            loggerM.warn("Znode {} is empty".format(args[0]))
             return
 
-        super(Mykazoo, self).delete(path, recursive=True)
+        super(Mykazoo, self).delete(args[0])
 
-    def set(self, path, value):
-        super(Mykazoo, self).set(path, value)
+    def rmr(self, *args):
+        loggerM.info('rmr path is {}'.format(args[0]))
+        if not self.exists(args[0]):
+            loggerM.warn("Znode {} is empty".format(args[0]))
+            return
 
-    def init(self, path):
+        super(Mykazoo, self).delete(args[0], recursive=True)
+
+    def create(self, *args):
+        loggerM.info('create path is {}'.format(args[0]))
+        if not self.exists(args[0]):
+            super(Mykazoo, self).create(args[0], args[1], makepath=True)
+        else:
+            loggerM.warn("Znode {} Already exists...".format(args[0]))
+
+    def add(self, *args):
+        loggerM.info('createAcl path is {} acl is {}'.format(args[0], self.default_acl))
+        if not self.exists(args[0]):
+            self.ensure_path(args[0])
+        else:
+            loggerM.warn("Znode {} Already exists...".format(args[0]))
+
+    def init(self, *args):
         # TODO i wanna init some config for zookeeper from config.json
-
         pass
 
     def usage(self):
         print("""Usage:
                  delete path
+                 rmr path
                  create path data
+                 add path
                  ls path
                  get path
+                 getAcl path
                  set path data
-                 init some data
+                 setAcl path
+                 init zk from config.json
               """)
 
 
@@ -94,7 +124,7 @@ class MyCompleter(object):
                 if idx:
                     path = origline[idx + 1:begin]
                 loggerM.debug('path :{}'.format(path))
-                candidates = self.zkServer.gls(path)
+                candidates = self.zkServer._gls(path)
                 loggerM.debug('candidates:'.format(candidates))
 
             if being_completed:
@@ -110,24 +140,28 @@ class MyCompleter(object):
         return response
 
 
-def utility(host, auth=None):
-    zk = Mykazoo(host, timeout=10)
+def utility(host, raw_auth_data=None, theme='digest'):
+    usrpasswd = raw_auth_data.split(':')
+    acl = [ks.make_digest_acl(usrpasswd[0], usrpasswd[1], all=True)]
+    zk = Mykazoo(host, timeout=10, default_acl=acl, auth_data=[(theme, raw_auth_data)])
     zk.start()
-    # TODO there is some thing wrong with auth
-    #  zk.add_auth('digest', auth)
     try:
         readline.parse_and_bind('tab: complete')
-        readline.set_completer(MyCompleter(COMMANDSET, zk).complete)
+        readline.set_completer(MyCompleter(COMMAND_SET, zk).complete)
         while True:
-            cmd = raw_input('>>').split()
+            cmd = raw_input('>> ').split()
             if not cmd:
                 continue
             elif cmd[0] == 'up':
                 break
-            elif cmd[0] in COMMANDSET:
+            elif cmd[0] in COMMAND_SET:
                 try:
-                    getattr(zk, cmd[0])(cmd[1])
+                    getattr(zk, cmd[0])(*cmd[1:])
                 except:
+                    type, value, tb = sys.exc_info()
+                    traceback.print_exc()
+                    pdb.post_mortem(tb)
+                    loggerM.warn('api {} call failed!!'.format(cmd[0]))
                     getattr(zk, 'usage')()
             else:
                 getattr(zk, 'usage')()
@@ -136,11 +170,20 @@ def utility(host, auth=None):
 
 
 def main():
-    connData = interacter(parseConfig())
-    utility(connData['server'], connData['auth'])
+    # TODO input zkconfig.json as param return index of config
+    # draw login screen based on return value of func load_config
+    # add func parser_config to parse the index get the connect data of zk
+    # draw a interacter screen(login) is very important
+    connData = interacter(load_config())
+    if connData:
+        utility(connData['server'], connData['auth'])
 
 
-def parseConfig():
+def parser_config(data):
+    pass
+
+
+def load_config():
     with open(CONFIG_FILE, 'r') as f:
         return json.load(f)
 
@@ -160,10 +203,10 @@ def interacter(menu):
     sumline = 0
     dictMenu = {}
     for title in menu:
-        screen.addstr(sumline, defaultY, '>> {}.{}\n'.format(sumline, title))
+        screen.addstr(sumline, defaultY, 'Hydra >> {}.{}\n'.format(sumline, title))
         dictMenu[sumline] = title
         sumline += 1
-    screen.addstr(sumline, defaultY, '>> {}.Quit'.format(sumline))
+    screen.addstr(sumline, defaultY, 'Hydra >> {}.Quit'.format(sumline))
     dictMenu[sumline] = 'Quit'
 
     cursor = 0
@@ -173,7 +216,7 @@ def interacter(menu):
             event = screen.getch()
             if event == curses.KEY_ENTER or event == 10:
                 if cursor == sumline:
-                    break
+                    return False
                 else:
                     return menu[dictMenu[cursor]]
             elif event == curses.KEY_UP:
@@ -191,10 +234,5 @@ def interacter(menu):
     finally:
         curses.endwin()
 
-
-def checkIllegal(host):
-    return True
-
 if __name__ == '__main__':
     main()
-    #  parseConfig()
